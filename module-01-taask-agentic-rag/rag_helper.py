8022063db99203d91ec592a181eb7a2caaee17d0
Malak Hisham <mallak.hisham.s@gmail.com>
2026-06-22 22:16:08 +0300

    uploading first task

---
+++ module-01-taask-agentic-rag/rag_helper.py
@@ -0,0 +1,70 @@
+INSTRUCTIONS = '''
+Your task is to answer questions from the course participants
+based on the provided context.
+
+Use the context to find relevant information and provide accurate
+answers. If the answer is not found in the context,
+respond with "I don't know."
+'''
+
+PROMPT_TEMPLATE = '''
+QUESTION: {question}
+
+CONTEXT:
+{context}
+'''.strip()
+
+
+class RAGBase:
+
+    def __init__(
+        self,
+        index,
+        llm_client,
+        instructions=INSTRUCTIONS,
+        prompt_template=PROMPT_TEMPLATE,
+        course='llm-zoomcamp',
+        model='gpt-5.4-mini'
+    ):
+        self.index = index
+        self.llm_client = llm_client
+        self.instructions = instructions
+        self.course = course
+        self.prompt_template = prompt_template
+        self.model = model
+
+    def search(self, query, num_results=5):
+
+        return self.index.search(
+            query,
+            num_results=num_results
+        )
+
+    def build_context(self, search_results):
+        lines = []
+
+        for doc in search_results:
+            lines.append(f"File: {doc['filename']}")
+            lines.append(doc['content'])
+            lines.append('')
+
+        return '\n'.join(lines).strip()
+
+    def build_prompt(self, query, search_results):
+        context = self.build_context(search_results)
+        return self.prompt_template.format(
+            question=query, context=context
+        )
+
+    def llm(self, prompt):
+        full_prompt = self.instructions + "\n\n" + prompt
+        response = self.llm_client.generate_content(full_prompt)
+        return response
+
+    def rag(self, query):
+        search_results = self.search(query)
+        prompt = self.build_prompt(query, search_results)
+        response = self.llm(prompt)
+        answer = response.text
+        input_tokens = response.usage_metadata.prompt_token_count
+        return answer, input_tokens
